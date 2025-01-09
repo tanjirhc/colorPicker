0baa41e9e353159ad9aef185ca0d8532d9e3d90a
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2025-01-09 23:52:08 +0600

    stateful color variable

--- src/ColorPicker.jsx
+++ src/ColorPicker.jsx
@@ -1,7 +1,15 @@
+import React, { useState } from "react";
 
+function ColorPicker() {
+  const [color, setColor] = useState("#FFFFFF");
 
-function ColorPicker(){
-
-  return(<></>)
+  return (<div className="color-picker-container">
+            <h1>Color Picker</h1>
+            <div className="color-display" style={{backgroundColor: color}}>
+              <p>Selected Color: {color}</p>
+            </div>
+            <label>Select a color</label>
+            <input type="color" value={color}/>
+          </div>);
 }
 export default ColorPicker;
